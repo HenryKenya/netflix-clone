7ea14b626adca8ac0ccb196231981d198850ae1d
Henry "Dru" Onyango <henry.onyango@outlook.com>
2020-01-06 13:53:50 +0300

    Add boostrap as a dependency, create landing & navbar components, add nav items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
